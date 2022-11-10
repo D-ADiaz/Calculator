1ca35464873c00d6a8aa589245b40aecffe9d907
Danny <ddiaz@scu.edu>
2022-11-10 01:22:50 -0800

    feat: Add operator function to select arithmetic

--- js-file.js
+++ js-file.js
@@ -15,3 +15,22 @@ function divide(num1,num2){
     answer = num2 === 0 ? "ERROR": answer;
     return answer;
 }
+
+function operate(num1,num2,operator){
+
+switch(operator)
+    {
+        case "+":
+            add(num1,num2);
+            break;
+        case "-":
+            subtract(num1,num2);
+            break;
+        case "*":
+            multiply(num1,num2);
+            break;
+        case "/":
+            divide(num1,num2);
+            break;
+    }
+}
